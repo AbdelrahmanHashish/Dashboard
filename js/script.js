16dea8a1b8253c1aa25f13ccae1969eea8809876
AbdelrahmanHashish <abdelrahmanhashish100@gmail.com>
2024-08-14 18:09:05 +0200

    Dashboard Dark Mode version 2

--- js/script.js
+++ js/script.js
@@ -1,21 +1,18 @@
 // JavaScript Document
 
-const sideMenu = document.querySelector('aside');
-const menuBtn = document.querySelector('#menu_bar');
-const closeBtn = document.querySelector('#close_btn');
-
-const themeToggler = document.querySelector('.theme-toggler');
-
-menuBtn.addEventListener('click',()=>{
-	sideMenu.style.display = "block";
-});
-
-closeBtn.addEventListener('click',()=>{
-	sideMenu.style.display = "none";
-});
-
-themeToggler.addEventListener('click',()=>{
-	document.body.classList.toggle('dark-theme-variables');
-	themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
-	themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+$(document).ready(function(){
+	
+	// show and hide menu
+	$("i#menu_icon").click(function(){
+		$("section#second_left_menu").slideToggle(1500);
+	});
+	
+	// make a search box in the Recent Order Table
+	$("input.search").keyup(function(){
+		var input_value = $(this).val().toLowerCase();
+		$("table#the_table tr.content").filter(function(){
+			$(this).toggle($(this).text().toLowerCase().indexOf(input_value) > -1);
+		});
+	});
+	
 });
